b33b07685684da1c4400653d038a55ee74600916
Achut Gite <achutgite3821@gmail.com>
2023-12-27 11:45:16 +0530

    27-12-23_small changes

--- src/pages/GeoConfigs.js
+++ src/pages/GeoConfigs.js
@@ -4,7 +4,7 @@ import Region from "../components/widgets/Region";
 import Level from "../components/widgets/Level";
 import { getApiDataFromAws, postApiDataToAws } from "../services/apis";
 import {
-  Select,Divider,Modal,Table,Form,Input,Button,Card,Col, Row,Spin
+  Select, Divider, Modal, Table, Form, Input, Button, Card, Col, Row, Spin
 } from "antd";
 import { useState } from "react";
 
@@ -35,7 +35,7 @@ function Config() {
 
   const dynamicColumns = (data) => {
     const dynamicColumns = [];
-  
+
     // Add logic here to extract dynamic column information from the data structure
     // For example, if "stateRef" exists in any of the items, add a column for it
     if (data.some(item => item.stateRef)) {
@@ -68,25 +68,25 @@ function Config() {
         onFilter: (value, record) => record.region.startsWith(value),
       })
     }
-  
+
     if (data.some(item => item.state)) {
-      dynamicColumns.push({    
-          title: "State",
-          dataIndex: "state",
-          key: "state",
-          sorter: (a, b) => a.state.localeCompare(b.state),
-          filters: Array.from(new Set(locationData.map(item => item.state))).map((name, index) => ({
-            text: name,
-            value: name,
-          })),
-          filterMode: "tree",
-          filterSearch: true,
-          onFilter: (value, record) => record.state.startsWith(value),
+      dynamicColumns.push({
+        title: "State",
+        dataIndex: "state",
+        key: "state",
+        sorter: (a, b) => a.state.localeCompare(b.state),
+        filters: Array.from(new Set(locationData.map(item => item.state))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.state.startsWith(value),
       })
     }
-  
+
     if (data.some(item => item.level)) {
-      dynamicColumns.push({ 
+      dynamicColumns.push({
         title: "Level",
         dataIndex: "level",
         key: "level",
@@ -98,10 +98,10 @@ function Config() {
         filterMode: "tree",
         filterSearch: true,
         onFilter: (value, record) => record.level.startsWith(value),
-       })
+      })
     }
     if (data.some(item => item.siteRef)) {
-      dynamicColumns.push({ 
+      dynamicColumns.push({
         title: "Site ",
         dataIndex: "siteRef",
         key: "siteRef",
@@ -113,10 +113,10 @@ function Config() {
         filterMode: "tree",
         filterSearch: true,
         onFilter: (value, record) => record.siteRef.startsWith(value),
-       })
+      })
     }
     if (data.some(item => item.projId)) {
-      dynamicColumns.push({ 
+      dynamicColumns.push({
         title: "Project ID",
         dataIndex: "projId",
         key: "projId",
@@ -130,10 +130,8 @@ function Config() {
         onFilter: (value, record) => record.projId.startsWith(value),
       })
     }
-  
     return dynamicColumns;
   };
-    
   const columns = [
     {
       title: "ID",
@@ -155,7 +153,6 @@ function Config() {
       onFilter: (value, record) => record.name.startsWith(value),
     },
     ...dynamicColumns(locationData),
-    
     {
       title: "Actions",
       dataIndex: "delete",
@@ -165,6 +162,7 @@ function Config() {
           <a
             onClick={() => {
               onEdit(record);
+              console.log(record)
             }}
           >
             EDIT
@@ -181,203 +179,202 @@ function Config() {
       ),
     },
   ];
-
   const changeWidgets = (widget) => {
     setActiveButton(widget);
     getData(widget);
   }
-
   const onDelete = async (id) => {
     try {
       const resp = await postApiDataToAws(id);
       getData();
     } catch (error) { }
   };
-
   const onEdit = async (record) => {
     form.setFieldsValue(record);
     //setSitesId(record.id);
     setOpen(true);
   };
-
   const getData = async (dataValues) => {
     setIsLoading(true);
     try {
 
       var locationData = [];
-      if(dataValues === 0){
+      if (dataValues === 0) {
         locationData = await getApiDataFromAws("queryType=state")
-      }else if(dataValues === 1){
+      } else if (dataValues === 1) {
         locationData = await getApiDataFromAws("queryType=region")
-      }else if(dataValues === 2){
+      } else if (dataValues === 2) {
         locationData = await getApiDataFromAws("queryType=level")
       }
-            
       setLocationData(locationData);
       setloading(false);
       setIsLoading(false);
     } catch (error) { }
   };
-
   useEffect(() => {
     getData(0);
     setActiveButton(0);
   }, []);
+  console.log(locationData)
 
+  const activeWidgestInputFields = () => {
+    switch (activeButton) {
+      case 0:
+        return (
+          <>
+            <Row justify={"center"}>
+              <Col span={24}>
+                <Form.Item
+                  name={"name"}
+                  label="State Name"
+                  // labelCol={{ span: 4 }}
+                  wrapperCol={{ span: 24 }}
+                >
+                  <Input className="form_input" />
+                </Form.Item>
+              </Col>
+            </Row>
+
+          </>
+        )
+      case 1:
+        return (
+          <>
+            <Row justify={"center"}>
+              <Col span={24}>
+                <Form.Item
+                  name={"name"}
+                  label="Region Name"
+                  // labelCol={{ span: 4 }}
+                  wrapperCol={{ span: 24 }}
+                >
+                  <Input className="form_input" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </>
+        )
+      case 2:
+        return (
+          <>
+            <Row justify={"center"}>
+              <Col span={24}>
+                <Form.Item
+                  name={"level"}
+                  label="Level Name"
+                  // labelCol={{ span: 4 }}
+                  wrapperCol={{ span: 24 }}
+                >
+                  <Input className="form_input" />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row justify={"center"}>
+              <Col span={24}>
+                <Form.Item
+                  name={"name"}
+                  label="Select Site Name"
+                  // labelCol={{ span: 4 }}
+                  wrapperCol={{ span: 24 }}
+                >
+                  <Select
+                    placeholder="Selct Site Name"
+                    style={{ width: "100" }}>
+                    {locationData.map((item, index) => (
+                      <Select.Option key={index} item={item.name}>{item.name}</Select.Option>
+                    ))}
+                  </Select>
+                  {/* <Input className="form_input" /> */}
+                </Form.Item>
+              </Col>
+            </Row>
+          </>
+        )
+    }
+  }
   return (
-        <>
-          <Row>
-            <Col span={17} style={{ marginBottom: 20 }}>
-              <Button type={activeButton === 0 ? 'primary' : 'button'} onClick={()=> changeWidgets(0)}>State</Button>
-              <Button type={activeButton === 1 ? 'primary' : 'button'} onClick={()=> changeWidgets(1)}>Region</Button>
-              <Button type={activeButton === 2 ? 'primary' : 'button'} onClick={()=> changeWidgets(2)}>Level</Button>
-              <Button className="mb-4 ml-4" type="primary" onClick={() => setOpen(true)}>
-                Create New
-              </Button>
-            </Col>
-            <Col span={7} style={{ marginBottom: 20 }}>
-              <Form>
-                <Input
-                  size="small"
-                  placeholder="search here ..."
-                  value={""}
-                // onChange={(e) => onChangeText(e.target.value)}
-                />
-              </Form>
-            </Col>
-          </Row>
-          <Modal
-            style={{ textAlign: "left" }}
-            title="Create New"
-            centered
-            open={open}
-            // onOk={() => setOpen(false)}
-            onCancel={() => setOpen(false)}
-            width={800}
-            footer={null}
-          >
-            <Form
-              className="modelForm"
-              {...layout}
-              name="nest-messages"
-              onFinish={onFinish}
-              validateMessages={validateMessages}
-              // style={{textAlign:'left'}}
-              labelAlign=""
-            >
-              <Form.Item
-                name={"energyMasterType"}
-                label="Energy Master Type"
-              >
-                <Select
-                  showSearch
-                  style={{
-                    width: 421,
-                  }}
-                  placeholder="Search to Select"
-                  optionFilterProp="children"
-                  filterOption={(input, option) =>
-                    (option?.label ?? "").includes(input)
-                  }
-                  filterSort={(optionA, optionB) =>
-                    (optionA?.label ?? "")
-                      .toLowerCase()
-                      .localeCompare(
-                        (optionB?.label ?? "").toLowerCase()
-                      )
-                  }
-                  options={[
-                    {
-                      value: "1",
-                      label: "Not Identified",
-                    },
-                    {
-                      value: "2",
-                      label: "Closed",
-                    },
-                    {
-                      value: "3",
-                      label: "Communicated",
-                    },
-                    {
-                      value: "4",
-                      label: "Identified",
-                    },
-                    {
-                      value: "5",
-                      label: "Resolved",
-                    },
-                    {
-                      value: "6",
-                      label: "Cancelled",
-                    },
-                  ]}
-                />
-                {/* <Input/> */}
-              </Form.Item>
-
-              <Form.Item label="Energy Type">
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label="Mesure"
-                style={
-                  {
-                    // width: 688,
-                    // display: "inline-block",
-                  }
-                }
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label="Unit"
-              // style={{
-              //   width: 158,
-              //   marginLeft:150,
-              // }}
-              >
-                <Input />
-              </Form.Item>
-
-              <Form.Item>
-                <Button
-                  style={{ float: "right", marginTop: 30 }}
-                  onClick={() => setOpen(false)}
-                  type=""
-                  htmlType="cancel"
-                >
-                  Cancel
-                </Button>
-                <Button
-                  className=""
-                  style={{
-                    float: "right",
-                    marginRight: 18,
-                    marginTop: 30,
-                  }}
-                  onClick={() => setOpen(false)}
-                  type="primary"
-                  htmlType="submit"
-                >
-                  Save
-                </Button>
-              </Form.Item>
-            </Form>
-          </Modal>
-          <Spin spinning={isLoading}>
-            <Table
-              bordered
-              columns={columns}
-              dataSource={locationData}
-              scroll={{
-                x: 1000,
-              }}
+    <>
+      <Row>
+        <Col span={17} style={{ marginBottom: 20 }}>
+          <Button type={activeButton === 0 ? 'primary' : 'button'} onClick={() => changeWidgets(0)}>State</Button>
+          <Button type={activeButton === 1 ? 'primary' : 'button'} onClick={() => changeWidgets(1)}>Region</Button>
+          <Button type={activeButton === 2 ? 'primary' : 'button'} onClick={() => changeWidgets(2)}>Level</Button>
+          <Button className="mb-4 ml-4" type="primary" onClick={() => setOpen(true)}>
+            Create New
+          </Button>
+        </Col>
+        <Col span={7} style={{ marginBottom: 20 }}>
+          <Form>
+            <Input
+              size="small"
+              placeholder="search here ..."
+              value={""}
+            // onChange={(e) => onChangeText(e.target.value)}
             />
-          </Spin>
-        </>
-
-
+          </Form>
+        </Col>
+      </Row>
+      <Modal
+        style={{ textAlign: "left" }}
+        title={activeButton==0?"Add New State":activeButton==1?"Add New Region":"Add New Level"}
+        centered
+        open={open}
+        // onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={700}
+        footer={null}
+      >
+        <Form
+          className="modelForm"
+          {...layout}
+          name="nest-messages"
+          style={{ minWidth: "100%" }}
+          layout="vertical"
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+          form={form}
+          labelAlign=""
+        >
+          {activeWidgestInputFields()}
+          <Row style={{alignItems:"end",display:"flex"}}>
+            <Col span={24}>
+          <Form.Item>
+            <Button
+              style={{ float: "right" }}
+              onClick={() => setOpen(false)}
+              type=""
+              htmlType="cancel"
+            >
+              Cancel
+            </Button>
+            <Button
+              className=""
+              style={{
+                float: "right",
+                marginRight: 18,
+              }}
+              onClick={() => setOpen(false)}
+              type="primary"
+              htmlType="submit"
+            >
+              Save
+            </Button>
+          </Form.Item>
+          </Col>
+          </Row>
+        </Form>
+      </Modal>
+      <Spin spinning={isLoading}>
+        <Table
+          bordered
+          columns={columns}
+          dataSource={locationData}
+          scroll={{
+            x: 1000,
+          }}
+        />
+      </Spin>
+    </>
   );
 }
 
