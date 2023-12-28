8f52206317fd7f4be638a70b523cf1e5701f5da8
Balaji Gite <balajigite16@gmail.com>
2023-12-28 21:41:09 +1100

    added some code for Targets

--- src/pages/Targets.js
+++ src/pages/Targets.js
@@ -15,6 +15,181 @@ function Targets() {
   const [form] = Form.useForm();
 
 
+  const dynamicColumns = (data) => {
+    const dynamicColumns = [];
+
+    // Add logic here to extract dynamic column information from the data structure
+    // For example, if "stateRef" exists in any of the items, add a column for it
+    if (data.some(item => item.elecTargetProfile)) {
+      dynamicColumns.push({
+        title: "Electricity Target Profile",
+        dataIndex: "elecTargetProfile",
+        key: "elecTargetProfile",
+        sorter: (a, b) => a.elecTargetProfile.localeCompare(b.elecTargetProfile),
+        filters: Array.from(new Set(targets.map(item => item.elecTargetProfile))).map((profile, index) => ({
+          text: profile,
+          value: profile,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.elecTargetProfile.startsWith(value),
+      });
+    }
+    if (data.some(item => item.targetKwh0)) {
+      dynamicColumns.push({
+        title: "Target kWh 0",
+        dataIndex: "targetKwh0",
+        key: "targetKwh0",
+        sorter: (a, b) => parseFloat(a.targetKwh0) - parseFloat(b.targetKwh0),
+        filters: Array.from(new Set(targets.map(item => item.targetKwh0))).map((kwh, index) => ({
+          text: kwh,
+          value: kwh,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetKwh0.startsWith(value),
+      })
+    }
+
+    if (data.some(item => item.targetKwh1)) {
+      dynamicColumns.push({
+        title: "Target kWh 1",
+        dataIndex: "targetKwh1",
+        key: "targetKwh1",
+        sorter: (a, b) => parseFloat(a.targetKwh1) - parseFloat(b.targetKwh1),
+        filters: Array.from(new Set(targets.map(item => item.targetKwh1))).map((kwh, index) => ({
+          text: kwh,
+          value: kwh,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetKwh1.startsWith(value),
+      })
+    }
+
+    if (data.some(item => item.targetKwh2)) {
+      dynamicColumns.push({
+        title: "Target kWh 2",
+        dataIndex: "targetKwh2",
+        key: "targetKwh2",
+        sorter: (a, b) => parseFloat(a.targetKwh2) - parseFloat(b.targetKwh2),
+        filters: Array.from(new Set(targets.map(item => item.targetKwh2))).map((kwh, index) => ({
+          text: kwh,
+          value: kwh,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetKwh2.startsWith(value),
+      })
+    }
+    if (data.some(item => item.waterTargetProfile)) {
+      dynamicColumns.push({
+        title: "Water Target Profile",
+        dataIndex: "waterTargetProfile",
+        key: "waterTargetProfile",
+        sorter: (a, b) => a.waterTargetProfile.localeCompare(b.waterTargetProfile),
+        filters: Array.from(new Set(targets.map(item => item.waterTargetProfile))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.waterTargetProfile.startsWith(value),
+      })
+    }
+    if (data.some(item => item.targetKl0)) {
+      dynamicColumns.push({
+        title: "Target Kl0",
+        dataIndex: "targetKl0",
+        key: "targetKl0",
+        sorter: (a, b) => a.targetKl0.localeCompare(b.targetKl0),
+        filters: Array.from(new Set(targets.map(item => item.targetKl0))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetKl0.startsWith(value),
+      })
+    }
+
+    if (data.some(item => item.targetKl1)) {
+      dynamicColumns.push({
+        title: "Target Kl1",
+        dataIndex: "targetKl1",
+        key: "targetKl1",
+        sorter: (a, b) => a.targetKl1.localeCompare(b.targetKl1),
+        filters: Array.from(new Set(targets.map(item => item.targetKl1))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetKl1.startsWith(value),
+      })
+    }
+    if (data.some(item => item.targetKl2)) {
+      dynamicColumns.push({
+        title: "Target Kl2",
+        dataIndex: "targetKl2",
+        key: "targetKl2",
+        sorter: (a, b) => a.targetKl2.localeCompare(b.targetKl2),
+        filters: Array.from(new Set(targets.map(item => item.targetKl2))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetKl2.startsWith(value),
+      })
+    }
+    if (data.some(item => item.gasTargetProfile)) {
+      dynamicColumns.push({
+        title: "Gas Target Profile",
+        dataIndex: "gasTargetProfile",
+        key: "gasTargetProfile",
+        sorter: (a, b) => a.gasTargetProfile.localeCompare(b.gasTargetProfile),
+        filters: Array.from(new Set(targets.map(item => item.gasTargetProfile))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.gasTargetProfile.startsWith(value),
+      })
+    }
+    if (data.some(item => item.targetCum0)) {
+      dynamicColumns.push({
+        title: "Target Cum0",
+        dataIndex: "targetCum0",
+        key: "targetCum0",
+        sorter: (a, b) => a.targetCum0.localeCompare(b.targetCum0),
+        filters: Array.from(new Set(targets.map(item => item.targetCum0))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetCum0.startsWith(value),
+      })
+    }
+    if (data.some(item => item.targetCum1)) {
+      dynamicColumns.push({
+        title: "Target Cum1",
+        dataIndex: "targetCum1",
+        key: "targetCum1",
+        sorter: (a, b) => a.targetCum1.localeCompare(b.targetCum1),
+        filters: Array.from(new Set(targets.map(item => item.targetCum1))).map((name, index) => ({
+          text: name,
+          value: name,
+        })),
+        filterMode: "tree",
+        filterSearch: true,
+        onFilter: (value, record) => record.targetCum1.startsWith(value),
+      })
+    }
+    return dynamicColumns;
+  };
 
   const columns = [
     {
@@ -50,32 +225,6 @@ function Targets() {
       onFilter: (value, record) => record.unit.startsWith(value),
     },
     {
-      title: "Electricity Target Profile",
-      dataIndex: "elecTargetProfile",
-      key: "elecTargetProfile",
-      sorter: (a, b) => a.elecTargetProfile.localeCompare(b.elecTargetProfile),
-      filters: Array.from(new Set(targets.map(item => item.elecTargetProfile))).map((profile, index) => ({
-        text: profile,
-        value: profile,
-      })),
-      filterMode: "tree",
-      filterSearch: true,
-      onFilter: (value, record) => record.elecTargetProfile.startsWith(value),
-    },
-    {
-      title: "Target kWh 0",
-      dataIndex: "targetKwh0",
-      key: "targetKwh0",
-      sorter: (a, b) => parseFloat(a.targetKwh0) - parseFloat(b.targetKwh0),
-      filters: Array.from(new Set(targets.map(item => item.targetKwh0))).map((kwh, index) => ({
-        text: kwh,
-        value: kwh,
-      })),
-      filterMode: "tree",
-      filterSearch: true,
-      onFilter: (value, record) => record.targetKwh0.startsWith(value),
-    },
-    {
       title: "Target Rating",
       dataIndex: "targetRating",
       key: "targetRating",
@@ -87,32 +236,6 @@ function Targets() {
       filterMode: "tree",
       filterSearch: true,
       onFilter: (value, record) => record.targetRating.startsWith(value),
-    },
-    {
-      title: "Target kWh 2",
-      dataIndex: "targetKwh2",
-      key: "targetKwh2",
-      sorter: (a, b) => parseFloat(a.targetKwh2) - parseFloat(b.targetKwh2),
-      filters: Array.from(new Set(targets.map(item => item.targetKwh2))).map((kwh, index) => ({
-        text: kwh,
-        value: kwh,
-      })),
-      filterMode: "tree",
-      filterSearch: true,
-      onFilter: (value, record) => record.targetKwh2.startsWith(value),
-    },
-    {
-      title: "Target kWh 1",
-      dataIndex: "targetKwh1",
-      key: "targetKwh1",
-      sorter: (a, b) => parseFloat(a.targetKwh1) - parseFloat(b.targetKwh1),
-      filters: Array.from(new Set(targets.map(item => item.targetKwh1))).map((kwh, index) => ({
-        text: kwh,
-        value: kwh,
-      })),
-      filterMode: "tree",
-      filterSearch: true,
-      onFilter: (value, record) => record.targetKwh1.startsWith(value),
     },
     {
       title: "Point",
@@ -166,6 +289,7 @@ function Targets() {
       filterSearch: true,
       onFilter: (value, record) => record.siteRef.startsWith(value),
     },
+    ...dynamicColumns(targets),
     {
       title: "Actions",
       dataIndex: "delete",
