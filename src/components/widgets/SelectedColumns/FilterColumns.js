06ae8a14917608248e47de39049baa70352b3116
Balaji Gite <balajigite16@gmail.com>
2024-03-24 20:08:21 +1100

    advance filter geoConfig

--- src/components/widgets/SelectedColumns/FilterColumns.js
+++ src/components/widgets/SelectedColumns/FilterColumns.js
@@ -1,92 +1,83 @@
 import { useState } from "react";
-import { Select,Input, Col, Row,} from "antd";
-import {  CaretDownOutlined, PlusOutlined, MinusOutlined } from "@ant-design/icons";
+import { Select, Input, Col, Row } from "antd";
+import { CaretDownOutlined, PlusOutlined, MinusOutlined } from "@ant-design/icons";
+import { message } from 'antd';
 
 const { Option } = Select;
 
-export const FilterColumnsData = ({ columns, isVisible }) => {
-    const [selectedColumns, setSelectedColumns] = useState([]);
-    const [rows, setRows] = useState([]);
+export const FilterColumnsData = ({ columns, isVisible, onSearch }) => {
+    const [rows, setRows] = useState([{ id: 1, selectValue: '', compareValue: "equalTo", inputValue: '', condition:'' }]);
 
     const addRows = () => {
-        if (rows.length < 5) {
+        if (rows.length < 3) {
             if (rows.length === 0) {
-                const newRow = { id: 1, selectValue: '', comparevalue: '', inputvalue: '' };
+                const newRow = { id: 1, selectValue: '', compareValue: "equalTo", inputValue: '', condition:'' };
                 setRows([newRow]);
             }
             else {
-                const newRow = { id: rows.length + 1, selectValue: '', comparevalue: '', inputvalue: '' };
+                const newRow = { id: rows.length + 1, selectValue: '', compareValue: "equalTo", inputValue: '', condition:'' };
                 setRows([...rows, newRow]);
             }
         }
     };
 
-    const handleInputChange = (id, event,) => {
+    const handleInputChange = (id, field, value) => {
         const updatedRows = rows.map(row =>
-            row.id === id ? { ...row, selectValue: event.target?.value, comparevalue: event.target?.value, inputvalue: event.target?.value } : row
+            row.id === id ? { ...row, [field]: value } : row
         );
         setRows(updatedRows);
     };
-    console.log(rows);
+
     const removeRows = (id) => {
         const updatedRows = rows.filter(row => row.id !== id);
         setRows(updatedRows);
     }
 
-    console.log(selectedColumns)
+    const handleSearch = () => {
+
+        const isValid = rows.every(row => row.selectValue && row.inputValue);
+
+        if (!isValid) {
+            message.error('Please fill in all required fields to apply search.');
+            return;
+        }
+
+        // Iterate over the rows dynamically to ensure the select condition of the first two rows is not empty
+        for (let i = 0; i < Math.min(2, rows.length); i++) {
+            if ( rows.length > 1 && !rows[i].condition) {
+                if((i+1) !=rows.length ){
+                    message.error(`The select condition of row ${i+1} must be selected.`);
+                    return;
+                }
+            }
+        }
+
+        const selectValues = rows.map(row => row.selectValue);
+        const inputValues = rows.map(row => row.inputValue);
+        const comapareValues = rows.map(row => row.compareValue);
+        const condition = rows.map(row => row.condition);
+        onSearch([selectValues, inputValues, comapareValues, condition]);
+    };
+
+    const handleClear = () =>{
+        setRows([])
+        onSearch("Reset");
+    }
+
     return (
         <>
-            <Row gutter={[12]}>
-                <Col span={6}>
-                    <Select
-                        style={{ width: '100%' }}
-                        placeholder="Select Parameters"
-                        value={selectedColumns}
-                        onChange={(selectedItems) => setSelectedColumns(selectedItems)}
-                        size="large"
-                    >
-                        {columns?.map((item, index) => (
-                            <Option key={item.key} value={item.title} style={{ color: "#8E8E8E" }}>
-                                {item.title}
-                            </Option>
-                        ))}
-                    </Select>
-                </Col>
-                <Col span={4}>
-                    <Select
-                        suffixIcon={<CaretDownOutlined />}
-                        style={{ width: "100%" }}
-                        size="large"
-                        defaultValue={"equal to"}
-                        options={[
-                            { value: '!', label: 'is not' },
-                            { value: '===', label: 'equal to' },
-                            { value: '!==', label: 'not equal to' },
-                            { value: '>', label: 'grether than' },
-                            { value: '<', label: 'less than' },
-                            { value: 'contains', label: 'contains' },
-                        ]}
-                    />
-                </Col>
-                <Col span={8}>
-                    <Input placeholder="Enter value" style={{ backgroundColor: 'transparent', border: "1px solid #8E8E8E4D" }} />
-                </Col>
-                <Col span={6} style={{ alignItems: 'center', display: 'flex' }}>
-                    <PlusOutlined style={{ color: '#FFFFFF', fontSize: '20px', border: "1px solid #8E8E8E4D", borderRadius: '5px', cursor: 'pointer' }} onClick={addRows} />
-                </Col>
-            </Row>
-            {rows.length > 0 && (rows.map(row => (
+            {rows.length > 0 && rows.map(row => (
                 <Row key={row.id} gutter={12} style={{ marginTop: '5px' }}>
                     <Col span={6}>
                         <Select
                             style={{ width: '100%' }}
                             placeholder="Select Parameters"
-
-                            onChange={(event) => handleInputChange(row.id, event, 1)}
+                            required
+                            onChange={(value) => handleInputChange(row.id, 'selectValue', value)}
                             size="large"
                         >
                             {columns?.map((item, index) => (
-                                <Option key={item.key} value={item.title} style={{ color: "#8E8E8E" }}>
+                                <Option key={item.key} value={item.key} style={{ color: "#8E8E8E" }}>
                                     {item.title}
                                 </Option>
                             ))}
@@ -98,32 +89,46 @@ export const FilterColumnsData = ({ columns, isVisible }) => {
                             style={{ width: "100%" }}
                             size="large"
                             defaultValue={"equal to"}
-                            onChange={(event) => handleInputChange(row.id, event)}
+                            onChange={(value) => handleInputChange(row.id, 'compareValue', value)}
                             options={[
-                                { value: '!', label: 'is not' },
-                                { value: '===', label: 'equal to' },
-                                { value: '!==', label: 'not equal to' },
-                                { value: '>', label: 'grether than' },
-                                { value: '<', label: 'less than' },
-                                { value: 'contains', label: 'contains' },
+                                { value: 'equalTo', label: 'equal to' },
+                                { value: 'notEqualTo', label: 'not equal to' },
+                                { value: 'includes', label: 'includes' },
+                                { value: 'greaterThan', label: 'greater than' },
+                                { value: 'lessThan', label: 'less than' },
                             ]}
                         />
                     </Col>
-                    <Col span={8}>
-                        <Input placeholder="Enter value" style={{ backgroundColor: 'transparent', border: "1px solid #8E8E8E4D" }} onChange={(event) => handleInputChange(row.id, event)} />
+                    <Col span={6}>
+                        <Input placeholder="Enter value" style={{ backgroundColor: 'transparent', border: "1px solid #8E8E8E4D" }} onChange={(e) => handleInputChange(row.id, 'inputValue', e.target.value)} required />
                     </Col>
-                    <Col span={6} style={{ alignItems: 'center', display: 'flex' }}><MinusOutlined style={{ color: '#FFFFFF', fontSize: '20px', border: "1px solid #8E8E8E4D", borderRadius: '5px', cursor: 'pointer', }} onClick={() => removeRows(row.id)} /></Col>
-                </Row>)))}
-            <Row gutter={[12]} style={{ marginTop: '10px' }}>
-                <Col span={8}>
-                    <button type="button" className="visible-button">Search</button>
-                    <button type="button" className="visible-button" style={{ backgroundColor: 'transparent', marginLeft: '10px' }} onClick={() => setRows([])}>Clear filter</button>
+                    <Col span={4}>
+                        <Select
+                            suffixIcon={<CaretDownOutlined />}
+                            style={{ width: "100%" }}
+                            size="large"
+                            placeholder="Select Condition"
+                            onChange={(value) => handleInputChange(row.id, 'condition', value)}
+                            options={[
+                                { value: 'and', label: 'and' },
+                                { value: 'or', label: 'or' },
+                            ]}
+                        />
+                    </Col>
+                    <Col span={4} style={{ alignItems: 'center', display: 'flex' }}>
+                        <MinusOutlined style={{ color: '#FFFFFF', fontSize: '20px', border: "1px solid #8E8E8E4D", borderRadius: '5px', cursor: 'pointer', }} onClick={() => removeRows(row.id)} />
+                    </Col>
+                </Row>
+            ))}
+            <Row gutter={[12]} style={{ marginTop: '15px' }}>
+                <Col span={12}>
+                    <button type="button" className="visible-button" onClick={addRows} style={{ backgroundColor: 'transparent', marginRight: '10px' }}>Add condition</button>
+                    <button type="button" className="visible-button" onClick={handleSearch}>Search</button>
+                    <button type="button" className="visible-button" style={{ backgroundColor: 'transparent', marginLeft: '10px' }} onClick={handleClear}>Clear filter</button>
                 </Col>
-                <Col span={8}></Col>
-                <Col span={8}></Col>
+                <Col span={6}></Col>
+                <Col span={6}></Col>
             </Row>
-
         </>
     );
 }
-
--- src/pages/GeoConfigs.js
+++ src/pages/GeoConfigs.js
@@ -285,6 +285,57 @@ function Config() {
     }
   }
 
+  const advancedFilterData = (value) =>{
+    if(value === "Reset"){
+      setLocationData(templocationData);
+    }else{
+      console.log(value);
+      let fillColumns = value[0];
+      let fillCondition = value[2];
+      let fillValue = value[1]; 
+      let fillAndOr = value[3];
+      
+      const filtersData = locationData.filter((record) => {
+        //record["name"].toLowerCase() === "nsw"
+        let query = '';
+        for (let i = 0; i < fillColumns.length; i++) {
+          let column = fillColumns[i];
+          let condition = fillCondition[i]
+          let value = fillValue[i]
+          let andor = i===0?'':fillAndOr[i-1]
+          if(condition === "equalTo"){
+            if(andor === "and"){
+              query == ''? query = record[column] === value:
+              query = query && record[column] === value 
+            }else{
+              query == ''? query = record[column] === value:
+              query = query || record[column] === value 
+            }
+          }else if(condition === "notEqualTo"){
+            if(andor === "and"){
+              query == ''? query = record[column] !== value:
+              query = query && record[column] !== value 
+            }else{
+              query == ''? query = record[column] !== value:
+              query = query || record[column] !== value 
+            }
+          }else if(condition === "includes"){
+            if(andor === "and"){
+              query == ''? query = record[column].toLowerCase().includes(value.toLowerCase()):
+              query = query && record[column].toLowerCase().includes(value.toLowerCase()) 
+            }else{
+              query == ''? query = record[column].toLowerCase().includes(value.toLowerCase()):
+              query = query || record[column].toLowerCase().includes(value.toLowerCase()) 
+            }
+          }
+        }
+        return query
+        
+      });
+      setLocationData(filtersData);
+    }
+  }
+
   const filterData = (value, data) => {
     const filtersData = data.filter((record) => (
       record.name.toLowerCase().includes(value.toLowerCase()) ||
@@ -604,7 +655,7 @@ function Config() {
         <div style={{ justifyContent: 'end', display: 'flex' }}>
           <CloseOutlined style={{ color: "#FFFFFF", fontSize: "15px", cursor: 'pointer' }} onClick={() => setVisibleCard(!visibleCard)} />
         </div>
-        <FilterColumnsData columns={columns} onSelectColumns={handleSelectColumns} />
+        <FilterColumnsData columns={columns} onSelectColumns={handleSelectColumns}  onSearch = {advancedFilterData}/>
       </Card>}
       <Modal
         style={{ textAlign: "left" }}
